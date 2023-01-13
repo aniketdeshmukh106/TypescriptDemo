d8d8cf2e594e030727b1c2b515d8d98bb96dc048
BharatBudhalani <bharat.budhalani@compumatrice.com>
2023-01-13 13:41:48 +0530

    Firstcommit

---
+++ TypeScriptDemos-main/examples/basic-types/scripts/basicTypes.ts
@@ -0,0 +1,26 @@
+﻿var x: number = 5;
+var y: number = 5;
+
+var isLoaded: boolean = false;
+
+var firstName: string = 'John';
+var lastName: string = 'Doe';
+
+var pets: string[] = ['Fido', 'Lassie', 'Rover'];
+
+var info: any = { address: '1234 Anywhere St.', city: 'Seattle' };
+
+window.onload = function () {
+    var z = x + y;
+    console.log('x + y = ' + z);
+    console.log('isLoaded = ' + isLoaded);
+    console.log(firstName + ' '+ lastName);
+    console.log('Pets List:');
+    // for (var i: number = 0; i < pets.length; i++) {
+    //     console.log(pets[i]);
+    // }
+    for(var ojk of pets){
+        console.log(ojk);
+    }
+    console.log('Info: ' + info.address + ' ' + info.city);
+};
